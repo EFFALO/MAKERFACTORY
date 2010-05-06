1b46efbe68762811bb1df1663c1083ec95f28f3d
Max Ogden <max@maxogden.com>
2010-05-05 20:56:28 -0700

    MAPZ

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,7 @@
 $(function(){
-
+  var map = new GMap2($("#map").get(0));
+  map.addMapType({type:G_AERIAL_MAP});
+  map.setMapType(G_AERIAL_MAP);
+  var portlandOR = new GLatLng(45.51330163230602, -122.67764210700989);
+  map.setCenter(portlandOR, 18);
 });
