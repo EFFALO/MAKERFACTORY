2d7cb6cfb300714c13ea48af6002c1e5ff5dc42e
Max Ogden <max@maxogden.com>
2010-05-05 22:17:02 -0700

    updating map center location

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,6 +2,6 @@ $(function(){
   var map = new GMap2($("#map").get(0));
   map.addMapType({type:G_AERIAL_MAP});
   map.setMapType(G_AERIAL_MAP);
-  var portlandOR = new GLatLng(45.51330163230602, -122.67764210700989);
+  var portlandOR = new GLatLng(45.51498682492308, -122.6799488067627);
   map.setCenter(portlandOR, 18);
 });
