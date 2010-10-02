12f5a72bb9264a6e6de8bd0cc368f066015c9836
Tonks Revelation <tonks@revelationglobal.com>
2010-10-01 23:38:52 -0700

    removed unused message/conversation models + tests

--- app/models/conversation.rb
+++
@@ -1,7 +0,0 @@
-class Conversation < ActiveRecord::Base
-  has_many :messages, :order => "created_at ASC"
-
-  def self.find_by_mail_id(mail_id)
-    self.find(:first, :joins => :messages, :conditions=> ['messages.mail_id = ?', mail_id], :readonly => false)
-  end
-end
--- spec/models/message_spec.rb
+++
@@ -1,8 +0,0 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
-
-describe Message do
-  it "should clean the email address on create" do
-    Factory.create(:message, :from => "--- - donkeys@bizzler.com  ")
-    Message.last.from.should == "donkeys@bizzler.com"
-  end
-end
