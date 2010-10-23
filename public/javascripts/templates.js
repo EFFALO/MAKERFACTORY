f6d6141137fe0110918064a14a7be5e68870f5a7
Michael Felix <mfelix@objcts.com>
2010-10-23 14:42:29 -0700

    styling map popup

--- public/javascripts/templates.js
+++ public/javascripts/templates.js
@@ -1,6 +1,6 @@
 makerFactory.templates = {
-  infoWindow : '{{#has_img}}<img src="{{img_url}}">{{/has_img}}' +
+  infoWindow : '<div class="popup">{{#has_img}}<img src="{{img_url}}">{{/has_img}}' +
     '<a href="{{href}}">{{title}}</a><br>' +
-    '{{location}}<br>' +
-    '{{quantity}}'
+    '<span>LOCATION:</span> {{location}}<br>' +
+    '<span>QUANTITY:</span> {{quantity}}</div>'
 };
