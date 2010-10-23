1efa86bfe039d4e389faaf7bbaaf8c1440ccc38d
Ezra Spier <Ezra.Spier@gmail.com>
2010-10-23 12:18:18 -0700

    Added 10 recent jobs to homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,8 @@ class PagesController < ApplicationController
   def home
     authorize! :access, :home
     @page_title = 'makerfactory'
+    @map_jobs = Job.all
+    @recent_jobs = Job.find(:all, :limit => 10, :order => 'created_at DESC')
   end
   
 end
