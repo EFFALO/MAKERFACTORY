b42bdca5d7f906ff0f054832128fe209f84b1131
Ezra Spier <Ezra.Spier@gmail.com>
2010-11-06 16:49:35 -0700

    configured errbit

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,6 +1,6 @@
 HoptoadNotifier.configure do |config|
    config.api_key = ENV['ERRBIT_KEY']
-   config.host    = 'mferrbit.heroku.com'
+   config.host    = ENV['ERRBIT_HOST']
    config.port    = 80 # Note: Deployment notifications only work on port 80
 end
 
