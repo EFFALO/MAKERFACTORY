7315655b0d76183248431f9fbe93fcb4642189c2
Ezra Spier <Ezra.Spier@gmail.com>
2010-11-06 17:55:24 -0700

    better initializer for errbit

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,6 +1,7 @@
-HoptoadNotifier.configure do |config|
-   config.api_key = ENV['ERRBIT_KEY']
-   config.host    = ENV['ERRBIT_HOST']
-   config.port    = 80 # Note: Deployment notifications only work on port 80
+if ENV['ERRBIT_HOST']
+  HoptoadNotifier.configure do |config|
+     config.api_key = ENV['ERRBIT_KEY']
+     config.host    = ENV['ERRBIT_HOST']
+     config.port    = 80 # Note: Deployment notifications only work on port 80
+  end
 end
-
