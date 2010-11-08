a767e0a7504c78b68e9ef15481fa480829205b53
Ezra Spier <Ezra.Spier@gmail.com>
2010-11-07 16:37:19 -0800

    fixed boolean logic in host check

--- config/initializers/refraction_rules.rb
+++ config/initializers/refraction_rules.rb
@@ -2,7 +2,7 @@ Refraction.configure do |req|
   mf_host = 'makerfactory.com'
   mf_staging = 'staging.makerfactory.com'
 
-  if req.host != (mf_host || mf_staging)
+  if((req.host != mf_staging) && (req.host != mf_host))
     req.permanent! :host => mf_host
   end
 end
