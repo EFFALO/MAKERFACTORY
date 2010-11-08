353c836f05719f1d5d7d102219f64baa6bd017e6
Ezra Spier <Ezra.Spier@gmail.com>
2010-11-07 16:22:03 -0800

    fix for redirect on staging

--- config/initializers/refraction_rules.rb
+++ config/initializers/refraction_rules.rb
@@ -1,7 +1,8 @@
 Refraction.configure do |req|
   mf_host = 'makerfactory.com'
+  mf_staging = 'staging.makerfactory.com'
 
-  if req.host != mf_host
+  if req.host != (mf_host || mf_staging)
     req.permanent! :host => mf_host
   end
 end
