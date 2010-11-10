e2921113fc2178a38d33733c26cb7c0748160235
Andrew Hay Kurtz <andrew.hay.kurtz@gmail.com>
2010-11-10 13:35:19 -0800

    amend last commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
   def home
     authorize! :access, :home
     @page_title = 'makerfactory'
-    @map_jobs = Job.active.find(:all, :order => 'created_at DESC')
+    @map_jobs = Job.active.find(:all, :order => 'created_at DESC', :conditions => 'lat IS NOT NULL')
     @recent_jobs =  @map_jobs.slice(0,10)
   end
   
