b5ffe89733373ac0b835af0ee67bafafba4dca85
Ezra Spier <ezra.spier@gmail.com>
2013-01-10 10:00:44 -0800

    Fix Rails security issue

--- config/application.rb
+++ config/application.rb
@@ -41,3 +41,6 @@ module MAKERFACTORY
 
   end
 end
+
+# https://groups.google.com/forum/?fromgroups=#!topic/rubyonrails-security/61bkgvnSGTQ
+ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::XML)
